e6144e0c801b64ce4bd413f05b41cbad24bbe0fd
mj3wc <wcronk@smu.edu>
2020-11-29 16:48:30 -0600

    added contractor API

---
+++ frontend/src/API/contractorRepo.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export class ContractorRepo{
+
+    url = 'http://localhost:8000'; //set up!!!!!!!!!! referenced my team's GUI from last year 
+    config = {
+
+    };
+};
