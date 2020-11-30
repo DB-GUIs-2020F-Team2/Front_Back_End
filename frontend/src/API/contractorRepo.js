e2f0a0072712fe0c22a5e879c96a362ea47b3c48
mj3wc <wcronk@smu.edu>
2020-11-29 18:50:00 -0600

    contractor directory

--- frontend/src/API/contractorRepo.js
+++ frontend/src/API/contractorRepo.js
@@ -2,8 +2,23 @@ import axios from 'axios';
 
 export class ContractorRepo{
 
-    url = 'http://localhost:8000'; //set up!!!!!!!!!! referenced my team's GUI from last year 
+    url = 'http://localhost:8000';
     config = {
 
     };
-};
+    
+    getDirectory(){
+        return new Promise((resolve,reject) =>{
+            axios.get(`${this.url}/getUser`, this.config)
+                .then(x => {
+                    resolve(x.data.data);
+                })
+                .catch(x => {
+                    alert(x);
+                    reject(x);
+                })
+        })
+    }
+    
+
+}
