6f1553d5e19b3e8af3e0b252bdc1cfb79659df71
mj3wc <wcronk@smu.edu>
2020-12-02 09:22:08 -0600

    updated register header

--- frontend/src/API/contractorRepo.js
+++ frontend/src/API/contractorRepo.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export class ContractorRepo{
     
-    //url = 'http://3.137.192.24:8000';
-    url = 'http://localhost:8000';
+    url = 'http://3.137.192.24:8000';
+   // url = 'http://localhost:8000';
     config = {
 
     };
